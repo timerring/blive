d628d67c49a4ad646dadef596301ee6e8a7289b9
John Howe <89397553+timerring@users.noreply.github.com>
2024-10-30 14:28:34 +0800

    feat: reupload the danmaku ready videos (#28)

--- upload/DanmakuProcess.sh
+++ upload/DanmakuProcess.sh
@@ -8,12 +8,17 @@ export assName
 # use DanmakuFactory to convert the xml file
 /root/blive/DanmakuFactory/DanmakuFactory -o "$assName" -i "$formatXmlName"
 
-#echo "$full_path"
-#echo "$assName"
-#echo "$formatVideoName"
+echo “danmaku convert success! And will delete xml files...”
+
+rm $formatXmlName
+# echo "$full_path"
+# echo "$assName"
+# echo "$formatVideoName"
 
 ffmpeg -i $full_path -vf ass=$assName $formatVideoName
 
-echo "ffmpeg successfully complete!"
+echo "ffmpeg successfully complete! And will delete danmaku files..."
+
+rm $assName
 
 ./uploadVideo.sh $formatVideoName
--- upload/operateStart.sh
+++ upload/operateStart.sh
@@ -14,14 +14,19 @@ while [ ${#queue[@]} -gt 0 ]; do
     queue=( "${queue[@]}" )
 
     for file in "$current_folder"/*; do
+        # echo "$file"
         if [ -d "$file" ]; then
             queue=( "${queue[@]}" "$file" )
         elif [[ $file == *.mp4 ]]; then
+            absolute_mp4_path=$(readlink -f "$file")
+            # echo "begin mp4 loop"
+	        echo "$absolute_mp4_path"
+            # echo "loop end"
             xml_file="${file%.mp4}.xml"
             if [ -f "$xml_file" ]; then
-	       absolute_mp4_path=$(readlink -f "$file")
-	       echo "$absolute_mp4_path"
-               /root/blive/biliup/formatName.sh "$absolute_mp4_path"
+                /root/blive/biliup/formatName.sh "$absolute_mp4_path"
+            elif [[ $file =~ ^/root/blive/Videos/[0-9]+/[0-9]+_[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}\.mp4$ ]]; then
+                /root/blive/biliup/uploadVideo.sh "$absolute_mp4_path"
             fi
         fi
     done
