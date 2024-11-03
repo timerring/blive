4a7e0087b24cffd71e2d6717663cef59591e8bc7
John Howe <89397553+timerring@users.noreply.github.com>
2024-11-03 19:41:23 +0800

    refactor: refactor the danmakufactory (#44)
    re #26

--- DanmakuFactory/VBGUI/DanmakuFactory/Define.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Module Define
-    Public Const cliVersion As String = "1.70"
-    Public Const guiVersion As String = "1.70"
-    Public Const guiVersionText As String = "1.70GUI Release(230825)"
-    Public Const cliProgramFileName As String = "DF_REL1.70CLI.exe"
-End Module
